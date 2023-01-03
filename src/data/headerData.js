b00c138120706507f622ad370a321a6b23ba26a1
@LISAN LE <105549298+Lisanle1@users.noreply.github.com>
2023-01-04 00:01:16 +0530

    update image

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ export const headerData = {
     name: 'Lisan M',
     title: "Hello! I am",
     desciption: "Hello! I am Lisan M, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/hMZ4s38/lisanphoto.jpg',
+    image: 'https://i.ibb.co/6wjfXSH/lisanphoto-2.jpg',
     resumePdf: 'https://drive.google.com/file/d/1WY9dswZpslGRSDpbYH74uUnLZZo2oVMd/view?usp=share_link'
 }     
  
