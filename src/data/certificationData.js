7e77911f5e8670870ebcb41549ccafbc6ae8385b
@LISAN LE <105549298+Lisanle1@users.noreply.github.com>
2023-02-18 11:42:41 +0530

    Update certificationData.js

--- src/data/certificationData.js
+++ src/data/certificationData.js
@@ -6,4 +6,11 @@ export const certificationData = [
         startYear: 'MAY 2022',
         endYear: 'Present'
     },
+      {
+        id: 2,
+        company: 'Guvi Geek Network Private Limited',
+        courseTitle: '15 Days Codekata Challenge',
+        startYear: '1 JAN 2023',
+        endYear: '15 JAN 2023'
+    },
 ]
