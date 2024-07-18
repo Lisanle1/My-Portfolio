b6731562efc8513926d5cd3d3d595bd665040279
Lisanle1 <lisanle31@gmail.com>
2024-07-18 11:03:10 +0530

    add profile photo and update resume url

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,11 @@
 
+import profile_photo from '../assets/profile_photo.jpg';
+
 export const headerData = {
     name: 'Lisan M',
     title: "Hello! I am",
     desciption: "Hello! I am Lisan M, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/6wjfXSH/lisanphoto-2.jpg',
-    resumePdf: 'https://drive.google.com/file/d/1FVZ6-9deDTpP6mDO-oWPwwQpTVbEKfZ9/view?usp=sharing'
+    image: profile_photo,
+    resumePdf: 'https://drive.google.com/file/d/1SUClbo9rZK9JWqejprM-uRNe_xG6QOH4/view?usp=sharing'
 }     
  
